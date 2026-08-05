1b5657f7ce0938e3f529c6d60647d1de5bbaa933
Lafe Spietz <lafe.f.spietz@nist.gov>
2026-08-05 15:35:53 -0600

    instrument.py is now a very bare bones debug script that just handles the json

--- instrument.py
+++ instrument.py
@@ -8,23 +8,25 @@ async def receive_data(websocket, state, previous_state):
     async for message in websocket:
         try:
             incoming_json = json.loads(message)
-            print(f"Incoming network data: {incoming_json}")
             
+            old_snapshot = copy.deepcopy(previous_state)
+            for key, new_value in incoming_json.items():
+                old_value = old_snapshot.get(key)
+                if old_value != new_value:
+                    print(f"Key '{key}' changed from {old_value} to {new_value}")
+
             state.clear()
             state.update(incoming_json)
-            
             if state != previous_state:
                 # WHERE ALL THE INSTRUMENTS WILL BE SET ON NETWORK CHANGE
                 previous_state.clear()
                 previous_state.update(copy.deepcopy(state))
-                
         except Exception as e:
             print(f"Error parsing data: {e}")
 
 async def main_loop(state, previous_state):
     async def connection_handler(ws):
         await receive_data(ws, state, previous_state)
-        
     async with websockets.serve(connection_handler, "localhost", 8080):
         while True:
             await asyncio.sleep(0.1)
@@ -32,13 +34,8 @@ async def main_loop(state, previous_state):
 if __name__ == "__main__":
     with open("state.json", "r") as f:
         state = json.load(f)
-    
     previous_state = copy.deepcopy(state)
-    
-    time.sleep(1)
-
-    # WHERE ALL THE INSTRUMENTS WILL BE SET ON BOOT (RUN ONCE)
-
+    time.sleep(1) # WHERE ALL THE INSTRUMENTS WILL BE SET ON BOOT (RUN ONCE)
     try:
         asyncio.run(main_loop(state, previous_state))
     except (KeyboardInterrupt, asyncio.CancelledError):
